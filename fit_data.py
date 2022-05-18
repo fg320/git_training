d11cc5305f6be5c6fcf1b82511ba4a9e9443c861
fg320 <fg320@ic.ac.uk>
2022-05-18 15:09:11 +0100

    Fit row data and change raw data as red dots

--- fit_data.py
+++ fit_data.py
@@ -1,7 +1,17 @@
 import numpy as np
 import matplotlib.pytplot as plt
+from scipy.optimize import curve_fit
 
 filename = "example_data.csv"
 data = np.loadtext(filename, delimiter=",", skiprows=1)
-plt.pot(data[:,0], data{:,1])
+plt.plot(data[:,0], data[:,1], "ro")
+
+def model(x, p1, p2):
+	return p1 * np.cos(p2 * x) + p2 * np.sin(p1 * x)
+
+popt, pcov = curve_fit(model, data[:,0], data[:,1], p0=(1,0.2))
+ 
+x - np.linspace(-2, 2.9, 100)
+plt.plot(x, model(x, *popt))
+
 plt.show()
