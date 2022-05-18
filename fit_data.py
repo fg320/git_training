9775ab943e0ffc50bfedf9d3e85b19ac6f7d3933
fg320 <fg320@ic.ac.uk>
2022-05-18 14:36:19 +0100

    Use temp variable

--- fit_data.py
+++ fit_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pytplot as plt
 
-data = np.loadtext("example_data.csv", delimiter=",", skiprows=1)
+filename = "example_data.csv"
+data = np.loadtext(filename, delimiter=",", skiprows=1)
 plt.pot(data[:,0], data{:,1])
 plt.show()
